35f42c51eeb73f2c4d5d0f4cbdb9dbada34f2a48
shiloh <lixiaolei595@gmail.com>
2024-07-19 17:16:10 +0800

    feat: wrap content into a scroll area

--- src/layouts/components/MainContent.vue
+++ src/layouts/components/MainContent.vue
@@ -5,66 +5,23 @@
 -->
 <template>
   <q-page-container>
-    <q-toolbar>
-      <q-breadcrumbs>
-        <q-breadcrumbs-el
-          label="Home"
-          icon="home"
-          to="/"
-        />
-        <q-breadcrumbs-el
-          v-for="breadcrumb in breadcrumbs"
-          :key="breadcrumb.label"
-          :label="breadcrumb.label"
-          :icon="breadcrumb.icon"
-        />
-      </q-breadcrumbs>
-    </q-toolbar>
-    <q-page padding>
-      <router-view />
+    <breadcrumb-view id="breadcrumb-view" />
+    <q-page class="row">
+      <div class="col-12">
+        <div class="column full-height">
+          <q-scroll-area class="col q-px-sm">
+            <router-view />
+          </q-scroll-area>
+        </div>
+      </div>
     </q-page>
   </q-page-container>
 </template>
 
 <script setup lang="ts">
-  import { onMounted, ref, watch } from 'vue'
-  import { useRouter } from 'vue-router'
+  import BreadcrumbView from 'layouts/components/BreadcrumbView.vue'
 
   defineOptions({ name: 'MainContent' })
-
-  export interface Breadcrumb {
-    icon?: string
-    label: string
-    to: string
-  }
-
-  const breadcrumbs = ref<Breadcrumb[]>([])
-
-  const router = useRouter()
-  const { currentRoute } = router
-  onMounted(() => {
-    createBreadcrumbs()
-  })
-
-  watch(
-    () => currentRoute.value,
-    () => {
-      createBreadcrumbs()
-    }
-  )
-
-  function createBreadcrumbs() {
-    breadcrumbs.value =
-      '/' === currentRoute.value.path
-        ? []
-        : [
-            {
-              label: currentRoute.value.name as string,
-              to: currentRoute.value.path,
-              icon: currentRoute.value.meta.icon as string
-            }
-          ]
-  }
 </script>
 
 <style scoped></style>
