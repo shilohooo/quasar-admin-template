2f9203d84f18f69fc9ac75f104906b2dd08e8708
shiloh <lixiaolei595@gmail.com>
2024-07-20 10:12:54 +0800

    refactor: remove console.log

--- src/stores/sidebar.ts
+++ src/stores/sidebar.ts
@@ -5,7 +5,6 @@ export const useSidebarStore = defineStore('sidebar', () => {
   const collapsed = ref(false)
 
   const toggleCollapsed = () => {
-    console.log('into')
     collapsed.value = !collapsed.value
     console.log(collapsed.value)
   }
