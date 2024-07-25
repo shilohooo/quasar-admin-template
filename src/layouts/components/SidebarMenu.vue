e2c8f59eb0171e8768a13ed109def91f9cd7bc9a
shiloh <lixiaolei595@gmail.com>
2024-07-25 11:45:54 +0800

    feat: display app log & system name on sidebar

--- src/layouts/components/SidebarMenu.vue
+++ src/layouts/components/SidebarMenu.vue
@@ -11,7 +11,7 @@
     bordered
     :breakpoint="500"
   >
-    <q-scroll-area class="fit">
+    <q-scroll-area style="height: calc(100% - 70px); margin-top: 70px">
       <q-list>
         <q-item
           clickable
@@ -20,7 +20,7 @@
           class="q-ma-sm"
           :active="HOME_MENU.path === route.path"
           active-class="bg-blue-1 rounded-borders"
-          @click="tabStore.addTab({ path: '/', label: 'Home', icon: 'home', type: MenuType.PAGE })"
+          @click="tabStore.addTab(HOME_MENU)"
         >
           <q-item-section avatar>
             <q-icon :name="HOME_MENU.icon" />
@@ -36,11 +36,27 @@
         </template>
       </q-list>
     </q-scroll-area>
+
+    <div
+      class="absolute-top q-pa-sm flex items-center justify-center"
+      style="height: 70px; gap: 10px"
+    >
+      <q-avatar
+        class="q-mb-sm"
+        color="white"
+      >
+        <img
+          src="~assets/logo.svg"
+          alt="App Logo"
+        />
+      </q-avatar>
+      <span class="text-weight-bold">Your System Name</span>
+    </div>
   </q-drawer>
 </template>
 
 <script setup lang="ts">
-  import { HOME_MENU, menuList, MenuType } from 'src/router/routes/menu.data'
+  import { HOME_MENU, menuList } from 'src/router/routes/menu.data'
   import SidebarMenuItem from 'layouts/components/SidebarMenuItem.vue'
   import { useSidebarStore } from 'stores/sidebar'
   import { useRoute } from 'vue-router'
