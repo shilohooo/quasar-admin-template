f11e85ead1a651dcaa06af33cc5b3e594c7c0232
shiloh <lixiaolei595@gmail.com>
2025-01-26 09:02:09 +0800

    feat(algolia): using q-input instead of default search-box widget

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -4,21 +4,35 @@
   * @date 2025/1/24 15:02
 -->
 <template>
-  <div>
-    <ais-instant-search :search-client="searchClient" :index-name="indexName" :insights="true">
-      <ais-configure :hits-per-page.camel="8" />
-      <!--      搜索框-->
-      <ais-search-box />
-      <!--      搜索结果列表-->
-      <ais-hits>
-        <template #item="{ item }">
-          <vue-json-pretty :data="item as unknown as JSONDataType" :deep="1" />
-        </template>
-      </ais-hits>
-      <!--      分页-->
-      <ais-pagination />
-    </ais-instant-search>
-  </div>
+  <ais-instant-search :search-client="searchClient" :index-name="indexName" :insights="true">
+    <ais-configure :hits-per-page.camel="8" />
+    <!--      搜索框-->
+    <ais-search-box class="q-mb-md">
+      <template #default="{ currentRefinement, isSearchStalled, refine }">
+        <q-input
+          autofocus
+          dense
+          outlined
+          placeholder="Filter search results..."
+          type="search"
+          :model-value="currentRefinement"
+          @update:model-value="(value) => refine(value)"
+        />
+        <span v-if="isSearchStalled">Loading...</span>
+      </template>
+    </ais-search-box>
+    <!--      搜索结果列表-->
+    <ais-hits>
+      <template #item="{ item }">
+        <!--          <vue-json-pretty :data="item as unknown as JSONDataType" :deep="1" />-->
+        <div>type: {{ item.type }}</div>
+        <div>url: {{ item.url }}</div>
+        <div style="word-break: break-word">content: {{ item.content }}</div>
+      </template>
+    </ais-hits>
+    <!--      分页-->
+    <ais-pagination />
+  </ais-instant-search>
 </template>
 
 <script setup lang="ts">
@@ -33,9 +47,9 @@ import {
 } from 'vue-instantsearch/vue3/es'
 import { liteClient as algoliasearch } from 'algoliasearch/lite'
 import 'instantsearch.css/themes/algolia-min.css'
-import VueJsonPretty from 'vue-json-pretty'
-import 'vue-json-pretty/lib/styles.css'
-import type { JSONDataType } from 'vue-json-pretty/types/utils'
+// import VueJsonPretty from 'vue-json-pretty'
+// import 'vue-json-pretty/lib/styles.css'
+// import type { JSONDataType } from 'vue-json-pretty/types/utils'
 
 // const indexName = ref('instant_search')
 // const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76')
